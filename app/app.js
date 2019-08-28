438fada5de794168598dc3069ebee02c4fd7d3ce
dxSpy <41914165+dxSpy@users.noreply.github.com>
2019-08-28 23:19:03 +0700

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,15 @@
+var params = window
+    .location
+    .search
+    .replace('?','')
+    .split('&')
+    .reduce(
+        function(p,e){
+            var a = e.split('=');
+            p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+            return p;
+        },
+        {}
+    );
+
+console.log( params['data']);
